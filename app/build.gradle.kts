bb0a5ae91965d824e1bc16a5cb98f389d4f4e693
bohdan547 <129954499+bohdan547@users.noreply.github.com>
2024-12-11 01:05:11 +0200

    done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.panevrn.favorite_english"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.panevrn.favorite_english"
